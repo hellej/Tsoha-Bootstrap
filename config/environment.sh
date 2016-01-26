880577290c102f64605b7cf0b10d78d082c5632d
Joose Helle <joose@Jooses-MacBook-Pro.local>
2016-01-26 14:29:31 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME=joohelle
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER=helle
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
