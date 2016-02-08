66e56f9bd6e28b0eb77d359082529d57519721ad
Joose Helle <joose@Jooses-MacBook-Pro.local>
2016-02-08 11:53:35 +0200

    dokmentaatioon lisätty tietokohteet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,16 +1,22 @@
 INSERT INTO Ryhma (nimi, kuvaus, luontiaika)
 VALUES('aktiivit', 'semmoset jotka tekee', NOW());
 
-INSERT INTO Kayttaja (ktunnus, nimi, sposti, salasana, yllapitaja, ryhma_id)
-VALUES('hellej', 'Joose Helle', 'helle@helsinki.fi', 'asdfj', TRUE, '1');
+INSERT INTO Kayttaja (ktunnus, nimi, sposti, salasana, yllapitaja)
+VALUES('hellej', 'Joose Helle', 'helle@helsinki.fi', 'asdfj', TRUE);
 
-INSERT INTO Lukija (kayttaja_id) VALUES(1);
+INSERT INTO Ryhmakayttaja (ryhma_id, kayttaja_id)
+VALUES(1,1);
+
+
 INSERT INTO Kirjoittaja (kayttaja_id) VALUES(1);
 
 INSERT INTO Aihe (nimi, luontiaika) VALUES('varasto', NOW());
 
-INSERT INTO Keskustelu(otsikko, aika, kirjoittaja_id, aihe_id) 
-VALUES('uusi kahvinkeitin', NOW(), 1, 1);
+INSERT INTO Keskustelu(otsikko, aika, kirjoittaja_id) 
+VALUES('uusi kahvinkeitin', NOW(), 1);
 
-INSERT INTO Viesti(sisalto, aika, keskustelu_id, kirjoittaja_id, lukija_id)
-VALUES('hankitaaks espressokone', NOW(), 1, 1, 1);
+INSERT INTO Keskusteluaihe(aihe_id, keskustelu_id)
+VALUES(1,1);
+
+INSERT INTO Vastine(sisalto, aika, keskustelu_id, kirjoittaja_id)
+VALUES('hankitaaks espressokone', NOW(), 1, 1);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -8,32 +8,37 @@ kuvaus varchar(400) NOT NULL,
 luontiaika timestamp
 );
 
-CREATE TABLE Ryhmakayttaja(
-id SERIAL PRIMARY KEY,
-ryhma_id INTEGER REFERENCES Ryhma(id)
-kayttaja_id INTEGER REFERENCES Kayttaja(id)
-);
-
 CREATE TABLE Kayttaja(
 id SERIAL PRIMARY KEY,
 ktunnus varchar(15) NOT NULL, 
 nimi varchar(30) NOT NULL,
 sposti varchar(30),
 salasana varchar(15) NOT NULL,
-yllapitaja boolean DEFAULT FALSE,
+yllapitaja boolean DEFAULT FALSE
 );
 
+CREATE TABLE Ryhmakayttaja(
+id SERIAL PRIMARY KEY,
+ryhma_id INTEGER REFERENCES Ryhma(id),
+kayttaja_id INTEGER REFERENCES Kayttaja(id)
+);
 
 CREATE TABLE Kirjoittaja(
 id SERIAL PRIMARY KEY,
 kayttaja_id INTEGER REFERENCES Kayttaja(id)
 );
 
-
 CREATE TABLE Aihe(
 id SERIAL PRIMARY KEY,
 nimi varchar(30),
 luontiaika date
+);
+
+CREATE TABLE Keskustelu(
+id SERIAL PRIMARY KEY,
+otsikko varchar(40) NOT NULL,
+aika timestamp,
+kirjoittaja_id INTEGER REFERENCES Kirjoittaja(id)
 );
 
 CREATE TABLE Keskusteluaihe(
@@ -42,19 +47,16 @@ aihe_id INTEGER REFERENCES Aihe(id),
 keskustelu_id INTEGER REFERENCES Keskustelu(id)
 );
 
-CREATE TABLE Keskustelu(
+CREATE TABLE Keskusteluaihe(
 id SERIAL PRIMARY KEY,
-otsikko varchar(40) NOT NULL,
-aika timestamp,
-kirjoittaja_id INTEGER REFERENCES Kirjoittaja(id),
+aihe_id INTEGER REFERENCES Aihe(id),
+keskustelu_id INTEGER REFERENCES Keskustelu(id)
 );
-
 
 CREATE TABLE Vastine(
 id SERIAL PRIMARY KEY,
 sisalto varchar(400) NOT NULL,
 aika timestamp,
 keskustelu_id INTEGER REFERENCES Keskustelu(id),
-kirjoittaja_id INTEGER REFERENCES Kirjoittaja(id),
-lukija_id INTEGER REFERENCES Lukija(id)
+kirjoittaja_id INTEGER REFERENCES Kirjoittaja(id)
 );
